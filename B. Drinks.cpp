0cce9ce0bc921bc8100884d0d9ccda90c4f60382
emtiaz1498 <101050100+emtiaz1498@users.noreply.github.com>
2022-05-27 17:28:45 +0600

    Add files via upload

---
+++ B. Drinks.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,a;
+double ans=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        ans = ans+a;
+
+    }
+cout<<ans/n;
+    return 0;
+}
